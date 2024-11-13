9184cf4188afdf0b27a7ac7adfb8daa268b4821f
Pawel Halka <arktykahalka@gmail.com>
2024-11-13 09:18:19 +0100

    Add inserting data from file, add uniqueConstrraint to tables

--- Database.py
+++ Database.py
@@ -1,6 +1,10 @@
+import csv
+from datetime import datetime
 from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, String, Float, Date, Integer, ForeignKey, MetaData
+from sqlalchemy import Table, Column, String, Float, Date, Integer, ForeignKey, UniqueConstraint, MetaData
 from sqlalchemy import update, select, delete, insert 
+
+csv_files = ['clean_measure.csv', 'clean_stations.csv']
 
 engine = create_engine('sqlite:///database.db')
 meta = MetaData()
@@ -15,18 +19,43 @@ with engine.connect() as connection:
                     Column('date', Date),
                     Column('precip', Float),
                     Column('tobs', Integer),
+                    UniqueConstraint('station', 'date', name='uix_station_date')
                 )
         
         stations = Table(
-                'stations', meta,
-                Column('id', Integer, primary_key = True),
-                Column('station', String, ForeignKey('measures.station')),
-                Column('latitude', Float),
-                Column('longitude', Float),
-                Column('elevation', Float),
-                Column('name',String),
-                Column('country', String),
-                Column('state', String),
-        )     
+                    'stations', meta,
+                    Column('id', Integer, primary_key = True),
+                    Column('station', String, ForeignKey('measures.station')),
+                    Column('latitude', Float),
+                    Column('longitude', Float),
+                    Column('elevation', Float),
+                    Column('name',String),
+                    Column('country', String),
+                    Column('state', String),
+                    UniqueConstraint('latitude', 'longitude', name='uix_latitude_longitude')
+                )     
 
         meta.create_all(engine)
+    else:
+        measures = Table('measures', meta, autoload_with = engine)
+        stations = Table('stations', meta, autoload_with = engine)
+
+    with open(csv_files[0], newline='', encoding='utf-8') as file:
+        csv_reader = csv.DictReader(file)
+        data = []
+
+        for row in csv_reader:
+            data.append({
+                    'station': (row['station']),
+                    'date' : datetime.strptime(row['date'], '%Y-%m-%d') if row['date'] else None,
+                    'precip' : float(row['precip']) if row['precip'] else None,
+                    'tobs' : int(row['tobs']) if row['tobs'] else None
+            })
+        with connection.begin() as transaction:
+            try:
+                connection.execute(insert(measures).prefix_with('OR IGNORE'), data)
+            except Exception as e:
+                print(f' Wystapil blad przy dopisywaniu: {e}')
+                transaction.rollback()
+
+
